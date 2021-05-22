1dfbaa9648a0704d710bb9c996f4223f2cb1f899
Tamer Wagih <mrtamerwagih@gmail.com>
2021-05-22 18:54:58 +0300

    Fixing some bugs

--- src/util.js
+++ src/util.js
@@ -31,6 +31,7 @@ export const prettyPrintStat = (stat) =>
 export const showDataOnMap = (data, casesType = 'cases') =>
   data.map((country) => (
     <Circle
+      key={Math.random() * 1000}
       center={[country.countryInfo.lat, country.countryInfo.long]}
       color={casesTypeColors[casesType].hex}
       fillColor={casesTypeColors[casesType].hex}
